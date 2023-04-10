ce7a3dcdd3c1074008d09085aeeaa7914aa1d304
Julio Gajardo Peredo <oilujulio@gmail.com>
2023-04-10 11:42:48 -0300

    first commit

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -6,20 +6,19 @@ vim.cmd.packadd('packer.nvim')
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+
+  use { "catppuccin/nvim", as = "catppuccin" }
 
   use({
       "folke/trouble.nvim",
@@ -39,7 +38,7 @@ return require('packer').startup(function(use)
   use("theprimeagen/harpoon")
   use("theprimeagen/refactoring.nvim")
   use("mbbill/undotree")
-  use("tpope/vim-fugitive")
+  -- use("tpope/vim-fugitive")
   use("nvim-treesitter/nvim-treesitter-context");
 
   use {
