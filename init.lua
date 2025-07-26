cecbb32eb492523c39be6d02f08c2d6274931262
Julio Gajardo Peredo <oilujulio@gmail.com>
2025-07-26 11:47:09 -0400

    FEAT: added copilot

--- init.lua
+++ init.lua
@@ -240,6 +240,31 @@ require('lazy').setup({
   -- ai code
   { 'codota/tabnine-nvim',    build = "./dl_binaries.sh" },
 
+
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = "<S-Tab>", -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  }
+
   --[[ {
     -- vim copilot
     -- 'github/copilot.vim'
